02194944a666747b40034c09e262c16d69024bcb
Amin Ahmadi <ahmadi.amin.a@gmail.com>
2021-04-30 15:29:29 +0200

    (i) add check boundary (ii) proper run of the system

--- src/circles.py
+++ src/circles.py
@@ -37,8 +37,11 @@ class Circle:
         xc, yc = self.x0, self.y0
         cond = ((x-xc)**2 + (y-yc)**2) <= self.get_radius()
         return cond
-    
-    def __check_boundary(self, x_bound, y_bound):
+
+    # TODO: two approaches to check boundary:
+    # (i) if space is available
+    # (ii) if boundaries touch.
+    def _check_boundary(self, x_bound, y_bound):
         """ Check if the circle circumference touches the boundary"""
         right_x = self.x0 + self.radius
         left_x = self.x0 - self.radius
@@ -50,30 +53,44 @@ class Circle:
 
         if (x_condition or y_condition):
             self.touch_boundary = True
+        return self.touch_boundary
 
 
 class Environment():
-    """ Class that control a system of circles in an environment"""
+    """ Class of environment controls the interaction of circles. """
 
     # TODO: initialize environment by an empty env
-    # TODO: check of circles are instatiated as object circle
+    # TODO: initialize env with definite number of circles at random potisions
+    # TODO: check if circles are instatiated as a circle object.
+    # TODO: check if circles are instatiated to be inside the boundary
     def __init__(self, circles, boundary):
         self.x_bound = boundary[0]
         self.y_bound = boundary[1]
         self.body = circles
 
     def _rn_generator(self, n_steps):
+        """Generate uniform random points defined by their cartesian
+        corrdinates that are bounded by boundary of the environment
+        """
         x_rand = rnd.uniform(size=n_steps, high=self.x_bound)
         y_rand = rnd.uniform(size=n_steps, high=self.y_bound)
         return x_rand, y_rand
     
     def get_state(self):
-        """ Return the current status of the system"""
+        """Return the current state of the system that is the location and
+        redius of all circles inside the environment.  
+        return: 
+        -------
+        state: np.array( (N,3) ) with N is number of circles and 3
+        represent (x,y,r)
+
+        """
         state = [list(c.get_position()+(c.get_radius(),)) for c in self.body]
-        # c.get_position()[0]for c in self.body
         return np.array(state)
 
     def _which_circle(self,x,y):
+        """Find list of circles that a given point (x,y) falls into the
+        circle."""
         return [c for c in self.body if c._is_in(x,y)]
 
     def run(self, n_steps: int, eps:float = 0.1):
@@ -83,15 +100,8 @@ class Environment():
         for x,y in zip(x_rand,y_rand):
             circles = self._which_circle(x,y)
             for c in circles:
-                c.expand(delta_r)
+                if not c._check_boundary(self.x_bound, self.y_bound):
+                    c.expand(delta_r)
     
-    # def run(self, time_steps=1000):
-    #     """ Run the system with given interaction between circles"""
-    #     for it in range(time_steps):
-    #         # TODO: define a process to run process()
-    #         # history[it] = self.get_state()
-    #         pass
-    #     pass
-            
     def get_history(self):
         pass
